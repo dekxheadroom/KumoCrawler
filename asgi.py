49116b9acbeb223a4e4fbcbb98b4edabfaa7cf87
dekx_headroom <derek.teo.sgp@gmail.com>
2025-05-24 00:01:57 +0800

    no need for asgi.py anymore

--- asgi.py
+++
@@ -1,5 +0,0 @@
-from a2wsgi import ASGIMiddleware
-from app import app # Imports the 'app' instance from your 'app.py'
-
-# This creates an ASGI-compatible application from your Flask app
-asgi_app = ASGIMiddleware(app)
